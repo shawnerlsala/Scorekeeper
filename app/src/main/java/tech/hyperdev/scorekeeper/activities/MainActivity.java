b1eed4401805f50596cd8abddffb2aa9c5c2c880
ShawnErl <shawnerlsala@bsit.usjr.edu.ph>
2017-07-18 23:51:37 +0800

    (4) Implement score counters.

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -1,15 +1,73 @@
 package tech.hyperdev.scorekeeper.activities;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context context;
+    TextView textView2;
+    ImageButton btnPlus;
+    ImageButton btnMinus;
+    TextView tvTeamName;
+    FragmentManager team1;
+    FragmentManager team2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView2 = (TextView)findViewById(R.id.textView2);
+        btnPlus = (ImageButton)findViewById(R.id.btnPlus);
+        btnMinus = (ImageButton) findViewById(R.id.btnMinus);
+        tvTeamName = (TextView)findViewById(R.id.tvTeamName);
+
+        context=this;
+        btnPlus.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                String present_value_string = textView2.getText().toString();
+                int present_value_int = Integer.parseInt(present_value_string);
+                present_value_int++;
+
+                textView2.setText(String.valueOf(present_value_int));
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                String present_value_string = textView2.getText().toString();
+                int present_value_int = Integer.parseInt(present_value_string);
+                present_value_int--;
+                if(present_value_int<=0)
+                {
+                    btnMinus.setEnabled(false);
+                }
+                else{
+                    btnMinus.setEnabled(true);
+
+                }
+
+                textView2.setText(String.valueOf(present_value_int));
+            }
+        });
+
     }
+
+
 }
+
--- app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -5,23 +5,53 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
 public class ScoreFragment extends Fragment {
 
+    private static ImageButton btnPlus;
+    private static ImageButton btnMinus;
+    private static TextView textView2;
+    private static TextView tvTeamName;
+    private static int team1;
+    private static int team2;
+
+    int a = 0;
+
     public ScoreFragment() {
-        // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_score, container, false);
 
+        btnPlus = (ImageButton) view.findViewById(R.id.btnPlus);
+        btnMinus = (ImageButton) view.findViewById(R.id.btnMinus);
+        textView2 = (TextView) view.findViewById(R.id.textView2);
+        tvTeamName = (TextView) view.findViewById(R.id.tvTeamName);
+
+        btnPlus.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View v){
+                        plusOnclick(v);
+                    }
+                }
+        );
+
+        return view;
+    }
+    public void plusOnclick(View v){
+        textView2.setText(""+a);
+    }
 }
